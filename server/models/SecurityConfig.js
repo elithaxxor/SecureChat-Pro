aef688f21182b710467aff702fc2200a67867fa0
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-09 05:01:44 -0400

    Create SecurityConfig.js

---
+++ server/models/SecurityConfig.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const SecurityConfigSchema = new mongoose.Schema({
+  encryptionParameters: {
+    algorithm: {
+      type: String,
+      default: 'aes-256-gcm'
+    },
+    keyRotationInterval: {
+      type: Number,
+      default: 3600 // 1 hour
+    }
+  },
+  passwordPolicy: {
+    minLength: {
+      type: Number,
+      default: 12
+    },
+    requireSpecialChar: Boolean,
+    historySize: {
+      type: Number,
+      default: 5
+    }
+  },
+  sessionConfig: {
+    idleTimeout: Number,
+    absoluteTimeout: Number
+  },
+  compliance: {
+    gdprRetention: Number,
+    hipaaCompliant: Boolean
+  }
+});
+
+export default mongoose.model('SecurityConfig', SecurityConfigSchema);
