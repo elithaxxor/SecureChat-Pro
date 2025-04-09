cf5f9d17428edd395fe09e4425c9f0f77022f048
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-09 05:49:27 -0400

    Create securityHeaders.js

---
+++ server/middleware/securityHeaders.js
@@ -0,0 +1,11 @@
+export const securityHeaders = (req, res, next) => {
+  res.set({
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+    'Content-Security-Policy': "default-src 'self'",
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'DENY',
+    'X-XSS-Protection': '1; mode=block',
+    'Referrer-Policy': 'no-referrer',
+  });
+  next();
+};
