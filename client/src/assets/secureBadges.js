dff16ef52ae0c76514f15c42a1b6420033da71e5
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-09 06:38:47 -0400

    Create secureBadges.js

---
+++ client/src/assets/secureBadges.js
@@ -0,0 +1,17 @@
+// Hashed Security Badges 
+
+export const SECURITY_ICONS = {
+  ENCRYPTED: {
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style="filter: drop-shadow(0 0 2px #2ecc71)">
+      <path fill="#27ae60" d="M12 17a2 2 0 0 0 2-2 2 2 0 0 0-2-2 2 2 0 0 0-2 2 2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5 5 5 0 0 1 5 5v2h1m-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3z"/>
+    </svg>`,
+    hash: "sha384-KUaw/dZ9RVpJ8f8y7Xk0gln4g9OK2N9bZ5W5DlK1Gl5b5b5b5b5b5b5b5b5b5b5b"
+  },
+
+  VERIFIED: {
+    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style="filter: drop-shadow(0 0 2px #3498db)">
+      <path fill="#2980b9" d="M23 12l-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 3 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5 12 21l3.4 1.5 1.89-3.19 3.61-.82-.34-3.7L23 12zm-12.91 4.72l-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"/> 
+    </svg>`,
+    hash: "sha384-5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5b5"
+  }
+};
